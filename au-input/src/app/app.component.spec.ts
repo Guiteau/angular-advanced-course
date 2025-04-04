5f839f8d92e06a52b1e8ab74d2c8e0f1dd160c1c
Guiteau <55487060+Guiteau@users.noreply.github.com>
2025-04-04 18:34:05 +0100

    feat: tests samples

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,56 +1,46 @@
-import {TestBed, async, ComponentFixture} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {DebugElement} from '@angular/core';
-import {By}              from '@angular/platform-browser';
-import {AuInputModule} from "./lib/au-input.module";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { DebugElement } from "@angular/core";
+import { AuInputModule } from "./lib/au-input.module";
+import { AuFaInputComponent } from "./lib/au-fa-input/au-fa-input.component";
+import { InputRefDirective } from "./lib/common/input-ref.directive";
+import { By } from "@angular/platform-browser";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let component: AppComponent,
+    fixture: ComponentFixture<AppComponent>,
+    el: DebugElement;
+  let emailField: DebugElement;
 
-    let component: AppComponent,
-        fixture: ComponentFixture<AppComponent>,
-        el: DebugElement,
-        emailField: DebugElement;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent, AuFaInputComponent, InputRefDirective],
+    }).compileComponents();
+  }));
 
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+    emailField = el.query(By.css("#email-field"));
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                AppComponent
-            ],
-            imports:[AuInputModule]
-        }).compileComponents();
-    }));
+    fixture.detectChanges();
+  }));
 
-    beforeEach(() => {
+  it("should create the app", async () => {
+    expect(component).toBeTruthy();
+  });
 
-        fixture = TestBed.createComponent(AppComponent);
-        component = fixture.debugElement.componentInstance;
-        el = fixture.debugElement;
-        emailField = el.query(By.css('#email-field'));
+  it("should create a font awesome email input", async(() => {
+    expect(emailField).toBeTruthy();
+  }));
 
-        fixture.detectChanges();
+  it("should include the correct email icon inside the email input", async(() => {
+    console.log(emailField.nativeElement.outerHTML);
+    expect(emailField.query(By.css("i.icon.fa.fa-envelope"))).toBeTruthy();
+  }));
 
-    });
-
-
-    it('should create the test application', async(() => {
-        expect(component).toBeTruthy();
-    }));
-
-
-    it('should create a font awesome email input', async(() => {
-        expect(emailField).toBeTruthy();
-
-    }));
-
-    it('should include the correct email icon inside the email input', async(() => {
-        expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
-    }));
-
-    it('should have projected the correct test input inside the email field', async(() => {
-        expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
-    }));
-
-
-
+  it("should have projected the correct test input inside the email field", async(() => {
+    expect(emailField.query(By.css("input.test-class"))).toBeTruthy();
+  }));
 });
